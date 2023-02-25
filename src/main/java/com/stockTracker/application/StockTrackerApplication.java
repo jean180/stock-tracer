5efb6f455017e98eee68fd9e49c5f3a1c98150de
jean180 <jean_2_0_0_0_@hotmail.com>
2023-02-25 01:26:57 +0100

    maquetación principal finalizada

--- src/main/java/com/stockTracker/application/StockTrackerApplication.java
+++ src/main/java/com/stockTracker/application/StockTrackerApplication.java
@@ -2,10 +2,18 @@ package com.stockTracker.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class StockTrackerApplication {
+public class StockTrackerApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(StockTrackerApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
