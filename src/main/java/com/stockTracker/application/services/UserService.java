5b66860a6c52fb0e2ae157d3d4304ff6f1d74aca
jean180 <jean_2_0_0_0_@hotmail.com>
2023-03-07 19:47:19 +0100

    Revert "Se sube un save de los avances, código fallido"
    This reverts commit 7bc6f3c044908cc5cbfac28c30b9b5ae7d3cf99f.

--- src/main/java/com/stockTracker/application/services/UserService.java
+++ src/main/java/com/stockTracker/application/services/UserService.java
@@ -3,7 +3,6 @@ package com.stockTracker.application.services;
 import com.stockTracker.application.entities.User;
 import com.stockTracker.application.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +11,7 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public void register(User user) throws Exception{
-        if (userRepository.findFirstByEmail(user.getEmail()) == null) {
-            userRepository.save(user);
-        } else {
-            throw new Exception("Usuario ya registrado");
-        }
+    public User register(User user) {
+        return userRepository.save(user);
     }
 }
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,42 +1,12 @@
-$(document).ready(function() {
-  //Recogemos el parámetro de error en caso de haberlo
-  var registerError = document.querySelector('[registerError]').getAttribute('registerError');
-  var registerAlert = $("#register-alert");
-  var userAlredyExistError = $("#userAlredyExistError");
-  
-  if(registerError!=null){
-    console.log(registerError);
-    registerAlert.removeClass("d-none");
-    userAlredyExistError.removeClass("d-none");
-  }
-
-  //Metodo para validar que los dos campos de la contraseña son iguales
-  var registerForm = document.getElementById("registerForm");
-  registerForm.addEventListener('submit', (event) => {
-    var password = $("#password");
-    var confirmPassword = $("#confirmPassword");
-    var registerAlert = $("#register-alert");
-    var passwordError = $("#passwordError");
-  
-    if (password!=confirmPassword) {
-      // La condición se cumple, cancelamos el envío del formulario
-      registerAlert.removeClass("d-none");
-      passwordError.removeClass("d-none");
-      event.preventDefault();
-    }
-  });
-
-});
-
 function tratarVisibilidadCards(buttonId) {
   var showLoginButton = $("#showLoginButton");
   var showRegisterButton = $("#showRegisterButton");
-  
+
   if (buttonId == 'showLoginButton') {
     showRegisterButton.removeClass("d-none");
     showLoginButton.addClass("d-none");
   }
-  
+
   if (buttonId == 'showRegisterButton') {
     showLoginButton.removeClass("d-none");
     showRegisterButton.addClass("d-none");
@@ -50,6 +20,6 @@ function moveDiv(buttonId) {
   var buttonWidth = button.width();
   var leftPosition = button.position().left;
   var newPosition = leftPosition < bodyWidth / 2 ? bodyWidth - buttonWidth : 0;
-  
+
   button.animate({ left: newPosition }, "slow");
 }
