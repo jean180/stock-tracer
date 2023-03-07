96ac8da030143d88bae43b76ac9a29018ae20313
jean180 <jean_2_0_0_0_@hotmail.com>
2023-03-08 00:36:27 +0100

    fix/ bloqueamos los botones hasta que el animate se cumpla, además refactorizamos código

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,8 +1,22 @@
+$(document).ready(function(){
+  $("#registerForm").submit(function (event) {
+    var password = $("#password");
+    var confirmPassword = $("#confirmPassword");
+    var userAlreadyExist = $("#userAlreadyExist-alert");
+    var registerAlert = $("#registerAlert");
+
+    if (password.val() != confirmPassword.val()) {
+      registerAlert.removeClass("d-none");
+      userAlreadyExist.addClass("d-none");
+  
+      event.preventDefault();
+    }
+  });
+});
 function tratarVisibilidadCards(buttonId) {
   //Variables
   var showLoginButton = $("#showLoginButton");
   var showRegisterButton = $("#showRegisterButton");
-  var registerAlert = $("#registerAlert");
 
   if (buttonId == 'showLoginButton') {
     showRegisterButton.removeClass("d-none");
@@ -13,20 +27,11 @@ function tratarVisibilidadCards(buttonId) {
     showLoginButton.removeClass("d-none");
     showRegisterButton.addClass("d-none");
   }
+
+  deshabilitarBotones();
   moveDiv(buttonId);
 
-  $("#registerForm").submit(function (event) {
-    var password = $("#password");
-    var confirmPassword = $("#confirmPassword");
-    var userAlreadyExist = $("#userAlreadyExist-alert");
-
-    if (password.val() != confirmPassword.val()) {
-      registerAlert.removeClass("d-none");
-      userAlreadyExist.addClass("d-none");
-
-      event.preventDefault();
-    }
-  });
+  
 }
 
 function moveDiv(buttonId) {
@@ -36,5 +41,19 @@ function moveDiv(buttonId) {
   var leftPosition = button.position().left;
   var newPosition = leftPosition < bodyWidth / 2 ? bodyWidth - buttonWidth : 0;
 
-  button.animate({ left: newPosition }, "slow");
+  button.animate({ left: newPosition }, {
+    duration: "slow", complete: function () {
+      habilitarBotones();
+    }
+  });
+}
+
+function deshabilitarBotones() {
+  $("#showLoginButton").addClass("disabled");
+  $("#showRegisterButton").addClass("disabled");
+}
+
+function habilitarBotones() {
+  $("#showLoginButton").removeClass("disabled");
+  $("#showRegisterButton").removeClass("disabled");
 }
