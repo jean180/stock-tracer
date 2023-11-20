8ef0c3a15d57dd089f0cc4e0206ca6f770a1a0f6
jean180 <jean_2_0_0_0_@hotmail.com>
2023-11-20 14:40:31 +0100

    Update products.js
    fix - Se soluciona typo en variable de js

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,16 +1,16 @@
-var idProcuts = [];
+var idProducts = [];
 function addRemoveProduct(productId) {
     var plusIcon = $("#plusIcon-" + productId);
     var minusIcon = $("#minusIcon-" + productId);
-    if (idProcuts.indexOf(productId) === -1) {
+    if (idProducts.indexOf(productId) === -1) {
         //Si el producto no está en la lista lo agregamos
-        idProcuts.push(productId);
+        idProducts.push(productId);
         plusIcon.addClass("d-none");
         minusIcon.removeClass("d-none");
     } else {
         //Si el producto está en la lista, lo eliminamos
         const index = idProcuts.findIndex(num => num ===productId);
-        idProcuts.splice(index, 1);
+        idProducts.splice(index, 1);
         minusIcon.addClass("d-none");
         plusIcon.removeClass("d-none");
     }
